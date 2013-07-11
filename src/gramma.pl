376575676682e11011ac6e482df515d333064702
Dmitry Andreevich <dmand@yandex.ru>
2013-07-11 17:29:20 +0300

    A bit of code cleanup

--- src/gramma.pl
+++ src/gramma.pl
@@ -3,17 +3,17 @@
  * Basic Lisp grammar and parser. Builds some kind of syntax tree.
  * Main non-terminal: sexpr.
  * Example of usage:
- * ?- sexpr(X, "(hello world of (sexpressions) 123 \"yey\")", []).
+ * ?- parse_sexpr("(hello world of (sexpressions) 123 \"yey\")", X).
  * 
  * X = sexpression([id([104, 101, 108, 108, 111]), id([119, 111, 114, 108, 100]), id([111, 102]), sexpression([id([115|...])]), number_lit(123), string_lit([121|...])])
  */ 
 
 % basic building blocks
-space    --> [X], { code_type(X, white) }.
+space    --> [C], { code_type(C, white) }.
 digit(C) --> [C], { code_type(C, digit) }.
 ascii(C) --> [C], { code_type(C, ascii) }.
-alpha(L) --> [L], { code_type(L, alpha) }.
-alnum(L) --> [L], { code_type(L, alnum) }.
+alpha(C) --> [C], { code_type(C, alpha) }.
+alnum(C) --> [C], { code_type(C, alnum) }.
 
 % whitespace skipping stuff
 optspace   --> [].
@@ -28,9 +28,8 @@ content([T]) --> term(T).
 content([T | C]) --> term(T), whitespace, content(C).
 
 term(L) --> literal(L).
-term(I) --> identifier(I).
-%term(I) --> quotation(Q).
 term(S) --> sexpr(S).
+term(I) --> identifier(I).
 
 literal(N) --> number(N).
 literal(S) --> string_literal(S).
@@ -41,7 +40,7 @@ digits([D | Ds]) --> digit(D), digits(Ds).
 
 string_literal(Ss) --> [34], string_content(S), [34], { Ss = string_lit(S) }.
 string_content([]) --> [].
-string_content([C | CS]) --> ascii(C), string_content(CS).
+string_content([C | Cs]) --> ascii(C), string_content(Cs).
 
 % TODO: identifiers are now C/Pascal-flavoured, this should be refined
 identifier(Id) --> alpha(Lh), rest_of_id(Ls), { Id = id([Lh | Ls]) }.
