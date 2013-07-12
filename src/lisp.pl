fe9bc003974f6e2ffeff62faab79b99b44177b85
Dmitry Andreevich <dmand@yandex.ru>
2013-07-13 00:19:17 +0300

    Add basic prettyprinter for lisp code

--- src/lisp.pl
+++ src/lisp.pl
@@ -1,28 +1,17 @@
 :- module(lisp, [repl]).
 :- use_module(gramma).
 :- use_module(eval).
-/*
- * Major things to implement are:
- * - Recursive descent parser
- * - Data structure for lisp code
- * - Evaluator of cod
- */
+:- use_module(pretty).
 
-prettify(number_lit(Value), Pretty) :-
-    number_codes(Value, Pretty).
-prettify(string_lit(X), X).
-prettyfy(boolean_lit(true), "#t").
-prettyfy(boolean_lit(false), "#f").
-prettyfy(nil_lit, "#nil").
 
 repl :-
-    nl,
     format(">>> "),
     read_line_to_codes(user_input, String),
-    % format("Debug: Got string \"~s\"", [String]),
-    nl,
-    parse_sexpr(String, ST),
-    eval(ST, Result),
-    prettify(Result, Pretty),
-    format("Answer = ~s, (AST = ~w)", [Pretty, Result]),
+    parse_sexpr(String, SyntaxTree),
+    %format("Debug: parsed syntax tree ~w\n", [SyntaxTree]),
+    pretty:print(SyntaxTree, PrettyST),
+    format("Debug: parsed expression ~s\n", [PrettyST]),
+    eval(SyntaxTree, Result),
+    pretty:print(Result, PrettyResult),
+    format("Answer = ~s, (ST = ~w)\n", [PrettyResult, Result]),
     repl.
---
+++ src/pretty.pl
@@ -0,0 +1,26 @@
+:- module(pretty, [print/2]).
+/*
+ * Prettyprinter for Lisp AST
+ */ 
+
+print(sexpression(Terms), Result) :-
+    print_list(Terms, Content),
+    format_to_chars("(~s)", [Content], Result).
+
+print(number_lit(Value), Result) :-
+    number_codes(Value, Result).
+
+print(string_lit(Value), Result) :-
+    format_to_chars("\"~s\"", [Value], Result).
+
+print(boolean_lit(true), "#t").
+print(boolean_lit(false), "#f").
+print(nil_lit, "#nil").
+
+print(id(Name), Name).
+
+print_list([T], Result) :- print(T, Result).
+print_list([H | T], Result) :-
+    print_list(T, Rest),
+    print(H, Head),
+    format_to_chars("~s ~s", [Head, Rest], Result).
