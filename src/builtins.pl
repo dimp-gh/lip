2b613a3d0950d4dd93623622aa7b59a003bf54da
Dmitry Andreevich <dmand@yandex.ru>
2013-07-14 13:45:01 +0300

    Add a few new builtins (*, -, =)

--- src/builtins.pl
+++ src/builtins.pl
@@ -4,12 +4,34 @@
 
 % Here i'll declare all built-in-language functions, as '+', '-', 'or', 'and'
 
-apply(builtin(+), [X], X).
+apply(builtin(+), [X], X) :- X = number_lit(_).
 apply(builtin(+), [number_lit(Arg) | Args], number_lit(Value)) :-
     apply(builtin(+), Args, number_lit(Sum)),
     Value is Arg + Sum.
 
+apply(builtin(-), [number_lit(X)], number_lit(Result)) :-
+    Result is -X.
+apply(builtin(-), [number_lit(Arg) | Args], number_lit(Value)) :-
+    apply(builtin(+), Args, number_lit(Sum)),
+    Value is Arg - Sum.
+
+apply(builtin(*), [X], X) :- X = number_lit(_).
+apply(builtin(*), [number_lit(Arg) | Args], number_lit(Value)) :-
+    apply(builtin(*), Args, number_lit(Product)),
+    Value is Arg * Product.
+
+apply(builtin(=), [H | T], boolean_lit(true)) :-
+    repetition(H, T).
+apply(builtin(=), [H | T], boolean_lit(false)) :-
+    not(repetition(H, T)).
+
+repetition(_, []).
+repetition(El, [El | T]) :-
+    repetition(El, T).
+
 gen_environ(Environ) :-
-    pairs_keys_values(Pairs, ["+"], [builtin(+)]),
+    pairs_keys_values(Pairs,
+		      ["+", "-", "*", "="],
+		      [builtin(+), builtin(-), builtin(*), builtin(=)]),
     list_to_assoc(Pairs, Environ).
     
--- tests/builtins_test.pl
+++ tests/builtins_test.pl
@@ -13,4 +13,50 @@ test('(+ 1 2 3 4)') :-
 	  ],
 	  number_lit(10)), !.
 
+test('(= 1 1 1 2)') :-
+    apply(builtin(=),
+	  [
+	   number_lit(1),
+	   number_lit(1),
+	   number_lit(1),
+	   number_lit(2)
+	  ],
+	  boolean_lit(false)).
+
+test('(= 0 0)') :-
+    apply(builtin(=),
+	  [
+	   number_lit(0),
+	   number_lit(0)
+	  ],
+	  boolean_lit(true)), !.
+
+test('(- 5)') :-
+    apply(builtin(-),
+	  [number_lit(5)],
+	  number_lit(-5)), !.
+
+test('(- 10 1 2 3 4)') :-
+    apply(builtin(-),
+	  [
+	   number_lit(10),
+	   number_lit(1),
+	   number_lit(2),
+	   number_lit(3),
+	   number_lit(4)
+	  ],
+	  number_lit(0)), !.
+
+test('(* 1 2 3 4 5)') :-
+    apply(builtin(*),
+	  [
+	   number_lit(1),
+	   number_lit(2),
+	   number_lit(3),
+	   number_lit(4),
+	   number_lit(5)
+	  ],
+	  number_lit(120)), !.
+
+
 :- end_tests(builtins).
