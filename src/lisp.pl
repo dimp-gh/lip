c36d848a366d3b87234e42836377cdbf951d549e
Dmitry Andreevich <dmand@yandex.ru>
2013-08-08 17:32:00 +0300

    Restructure main REPL module

--- src/lisp.pl
+++ src/lisp.pl
@@ -4,20 +4,27 @@
 :- use_module(pretty).
 
 
-repl :-
+read_expr(X) :-
     format(">>> "),
     read_line_to_codes(user_input, String),
     parse_repl(String, SyntaxTree),
     %format("Debug: parsed syntax tree ~w\n", [SyntaxTree]),
-    pretty:print(SyntaxTree, PrettyST),
-    format("Debug: parsed expression ~s\n", [PrettyST]),
-    eval(SyntaxTree, Result),
-    pretty:print(Result, PrettyResult),
-    format("Answer = ~s, (ST = ~w)\n", [PrettyResult, Result]),
+    %pretty:print(SyntaxTree, PrettyST),
+    %format("Debug: parsed expression ~s\n", [PrettyST]),
+    X = SyntaxTree.
+
+print(X) :-
+    pretty:print(X, Pretty),
+    format("~s\n", [Pretty]).
+
+repl :-
+    read_expr(SyntaxTree),
+    eval_safe(SyntaxTree, Result),
+    print(Result),
     repl.
 
 interp(Filename) :-
     atom_codes(FileAtom, Filename),
     read_file_to_codes(FileAtom, Content, []),
     parse_module(Content, SyntaxTree),
-    eval(SyntaxTree, _), !.
+    eval_safe(SyntaxTree, _), !.
